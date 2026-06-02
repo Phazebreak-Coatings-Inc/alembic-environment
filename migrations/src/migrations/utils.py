538bd7bb219101e35e8ed01fbe3b6d293ab42906
miles <miles.c.luce@gmail.com>
2026-06-02 13:43:41 -0500

    environment setup

--- migrations/src/migrations/utils.py
+++ migrations/src/migrations/utils.py
@@ -1,3 +1,26 @@
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Engine
+from pydantic_settings import BaseSettings
 
 APP_METADATA = MetaData()
+
+class BaseDatabaseSettings(BaseSettings):
+    database_port: str
+    database_username: str
+    database_password: str
+
+    @property
+    def database_url(self) -> str:
+        return ""
+
+    @property
+    def engine(self):
+        return Engine()
+
+class DevDatabaseSettings():
+    ...
+
+class StagingDatabaseSettings():
+    ...
+
+class ProdDatabaseSettings():
+    ...
