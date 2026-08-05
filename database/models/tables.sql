19fb9bd001f7b277c0826fdcddf76bf9ab0f54a3
Miles Copeland Luce <miles.c.luce@gmail.com>
2026-08-05 13:39:47 -0500

    adding a new table

--- database/models/tables.sql
+++ database/models/tables.sql
@@ -1,7 +1,16 @@
 CREATE TABLE users (
   user_id INT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL UNIQUE,    
+  username VARCHAR(50) NOT NULL UNIQUE,
   email VARCHAR(100),
   password VARCHAR(100),
-  join_date DATE DEFAULT CURRENT_TIMESTAMP 
+  join_date DATE DEFAULT CURRENT_TIMESTAMP
+  -- created_by VARCHAR
+  -- last_modified_by VARCHAR
+);
+
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY,
+  title VARCHAR(500),
+  description VARCHAR(2000),
+  user_id INT NOT NULL REFERENCES users(user_id)
 );
